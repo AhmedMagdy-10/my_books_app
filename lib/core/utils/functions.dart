e97c42b3f80b0e5a888999716f8dc623ee3780f7
AhmedMagdy-10 <ahmedmagdy332002@gmail>
2025-12-23 07:24:59 +0200

    create newest books use case

--- lib/core/utils/functions.dart
+++ lib/core/utils/functions.dart
@@ -1,8 +1,14 @@
+import 'package:books_app/core/errors/failure.dart';
 import 'package:books_app/core/utils/constants.dart';
 import 'package:books_app/feature/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 import 'package:hive/hive.dart';
 
 void savedLocalData(List<BookEntity> books, String booksName) {
   var box = Hive.box(boxName);
   box.addAll(books);
+}
+
+abstract class UseCase<type> {
+  Future<Either<Failure, type>> call();
 }
--- lib/feature/home/domain/UseCase/get_newest_books_use_case.dart
+++ lib/feature/home/domain/UseCase/get_newest_books_use_case.dart
@@ -1,17 +1,16 @@
 import 'package:books_app/core/errors/failure.dart';
 import 'package:books_app/core/utils/functions.dart';
 import 'package:books_app/feature/home/domain/entities/book_entity.dart';
-
 import 'package:books_app/feature/home/domain/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class GetMainBooksUseCase extends UseCase<List<BookEntity>> {
+class GetNewestBooksUseCase extends UseCase<List<BookEntity>> {
   final HomeRepo homeRepo;
 
-  GetMainBooksUseCase({required this.homeRepo});
+  GetNewestBooksUseCase({required this.homeRepo});
 
   @override
   Future<Either<Failure, List<BookEntity>>> call() {
-    return homeRepo.fetchFeaturedBooks();
+    return homeRepo.fetchNewestBooks();
   }
 }
