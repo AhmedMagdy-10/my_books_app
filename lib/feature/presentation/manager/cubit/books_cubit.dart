ad180e5145205b1fabc9541e53f5102c0bd06922
AhmedMagdy-10 <ahmedmagdy332002@gmail>
2025-12-23 11:46:44 +0200

    impelement newest books cubit

--- lib/feature/presentation/manager/cubit/books_cubit.dart
+++ lib/feature/presentation/manager/cubit/books_cubit.dart
@@ -1,15 +1,29 @@
 import 'package:books_app/feature/home/domain/UseCase/get_main_books_use_case.dart';
+import 'package:books_app/feature/home/domain/UseCase/get_newest_books_use_case.dart';
 import 'package:books_app/feature/presentation/manager/cubit/books_cubit_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BooksCubit extends Cubit<BooksCubitStates> {
-  BooksCubit(this.getMainBooksUseCase) : super(BooksInitialState());
+  BooksCubit({
+    required this.getMainBooksUseCase,
+    required this.getNewestBooksUseCase,
+  }) : super(BooksInitialState());
 
   final GetMainBooksUseCase getMainBooksUseCase;
-
+  final GetNewestBooksUseCase getNewestBooksUseCase;
   Future<void> getMainBooks() async {
     emit(BooksLoadingState());
     var results = await getMainBooksUseCase.call();
+    results.fold(
+      ((failure) =>
+          emit(BooksFailureState(errorMassege: failure.errorMessage))),
+      ((books) => emit(BooksSuccessState(books: books))),
+    );
+  }
+
+  Future<void> getNewestBooks() async {
+    emit(BooksLoadingState());
+    var results = await getNewestBooksUseCase.call();
     results.fold(
       ((failure) =>
           emit(BooksFailureState(errorMassege: failure.errorMessage))),
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:books_app/core/utils/constants.dart';
+import 'package:books_app/feature/home/data/data_source/home_local_data_source.dart';
+import 'package:books_app/feature/home/data/data_source/home_remote_data_source.dart';
+import 'package:books_app/feature/home/domain/UseCase/get_main_books_use_case.dart';
+import 'package:books_app/feature/home/domain/UseCase/get_newest_books_use_case.dart';
 import 'package:books_app/feature/home/domain/entities/book_entity.dart';
+import 'package:books_app/feature/home/domain/repo/home_repo.dart';
+import 'package:books_app/feature/home/domain/repo/home_repo_implemention.dart';
+import 'package:books_app/feature/presentation/manager/cubit/books_cubit.dart';
 import 'package:books_app/feature/presentation/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 
@@ -17,14 +25,33 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mainColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
-      ),
+    return BlocProvider(
+      create: (context) => BooksCubit(
+        getMainBooksUseCase: GetMainBooksUseCase(
+          homeRepo: HomeRepoImpl(
+            homeLocalDataSource: HomeLocalDataSourceImpl(),
+            homeRemoteDataSource: HomeRemoteDataSourceImpl(),
+          ),
+        ),
 
-      home: SplashView(),
+        getNewestBooksUseCase: GetNewestBooksUseCase(
+          homeRepo: HomeRepoImpl(
+            homeLocalDataSource: HomeLocalDataSourceImpl(),
+            homeRemoteDataSource: HomeRemoteDataSourceImpl(),
+          ),
+        ),
+      ),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mainColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+        ),
+
+        home: SplashView(),
+      ),
     );
   }
 }
